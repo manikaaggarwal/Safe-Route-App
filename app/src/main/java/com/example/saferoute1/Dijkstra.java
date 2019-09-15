63f431237807db733b63b7e3e63e9da60a5becc7
maniagga <maniagga@adobe.com>
2019-09-15 22:50:52 +0530

    final with colour

--- app/src/main/java/com/example/saferoute1/Dijkstra.java
+++ app/src/main/java/com/example/saferoute1/Dijkstra.java
@@ -1,58 +1,150 @@
 package com.example.saferoute1;
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+//import java.util.*;
+//import java.lang.*;
+//import java.io.*;
 
-public class Dijkstra {
 
-        static final int V = 9;
-        int minDistance(int dist[], Boolean sptSet[])
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class Dijkstra{
+
+    private static final int NO_PARENT = -1;
+    ArrayList<Integer> path = new ArrayList<Integer>();
+
+    // Function that implements Dijkstra's
+    // single source shortest path
+    // algorithm for a graph represented
+    // using adjacency matrix
+    // representation
+    public void dijkstraAlgo(int[][] adjacencyMatrix,
+                             int startVertex, int destIndex)
+    {
+        int nVertices = adjacencyMatrix[0].length;
+
+        // shortestDistances[i] will hold the
+        // shortest distance from src to i
+        int[] shortestDistances = new int[nVertices];
+
+        // added[i] will true if vertex i is
+        // included / in shortest path tree
+        // or shortest distance from src to
+        // i is finalized
+        boolean[] added = new boolean[nVertices];
+
+        // Initialize all distances as
+        // INFINITE and added[] as false
+        for (int vertexIndex = 0; vertexIndex < nVertices;
+             vertexIndex++)
         {
-            int min = Integer.MAX_VALUE, min_index = -1;
-            for (int v = 0; v < V; v++)
-                if (sptSet[v] == false && dist[v] <= min) {
-                    min = dist[v];
-                    min_index = v;
+            shortestDistances[vertexIndex] = Integer.MAX_VALUE;
+            added[vertexIndex] = false;
+        }
+
+        // Distance of source vertex from
+        // itself is always 0
+        shortestDistances[startVertex] = 0;
+
+        // Parent array to store shortest
+        // path tree
+        int[] parents = new int[nVertices];
+
+        // The starting vertex does not
+        // have a parent
+        parents[startVertex] = NO_PARENT;
+
+        // Find shortest path for all
+        // vertices
+        for (int i = 1; i < nVertices; i++)
+        {
+
+            // Pick the minimum distance vertex
+            // from the set of vertices not yet
+            // processed. nearestVertex is
+            // always equal to startNode in
+            // first iteration.
+            int nearestVertex = -1;
+            int shortestDistance = Integer.MAX_VALUE;
+            for (int vertexIndex = 0;
+                 vertexIndex < nVertices;
+                 vertexIndex++)
+            {
+                if (!added[vertexIndex] &&
+                        shortestDistances[vertexIndex] <
+                                shortestDistance)
+                {
+                    nearestVertex = vertexIndex;
+                    shortestDistance = shortestDistances[vertexIndex];
                 }
+            }
 
-            return min_index;
+            // Mark the picked vertex as
+            // processed
+            added[nearestVertex] = true;
+
+            // Update dist value of the
+            // adjacent vertices of the
+            // picked vertex.
+            for (int vertexIndex = 0;
+                 vertexIndex < nVertices;
+                 vertexIndex++)
+            {
+                int edgeDistance = adjacencyMatrix[nearestVertex][vertexIndex];
+
+                if (edgeDistance > 0
+                        && ((shortestDistance + edgeDistance) <
+                        shortestDistances[vertexIndex]))
+                {
+                    parents[vertexIndex] = nearestVertex;
+                    shortestDistances[vertexIndex] = shortestDistance +
+                            edgeDistance;
+                }
+            }
         }
 
-        // A utility function to print the constructed distance array
-//        void printSolution(int dist[], int n)
-//        {
-//            System.out.println("Vertex Distance from Source");
-//            for (int i = 0; i < V; i++)
-//                System.out.println(i + " tt " + dist[i]);
-//        }
+        printSolution(startVertex, shortestDistances, parents,  destIndex);
+    }
+
+    // A utility function to print
+    // the constructed distances
+    // array and shortest paths
+    private  void printSolution(int startVertex,
+                                      int[] distances,
+                                      int[] parents, int destIndex)
+    {
+//        int nVertices = distances.length;
+//        System.out.print("Vertex\t Distance\tPath");
+
+        int vertexIndex = destIndex;
+        Log.e("startVertex " , startVertex + " -> ");
+        Log.e("destIndex " ,vertexIndex + " \t\t ");
+        Log.e(" distances " ,distances[vertexIndex] + "\t\t");
+                printPath(vertexIndex, parents);
 
 
-        void dijkstra(int graph[][], int src)
+    }
+
+    // Function to print shortest path
+    // from source to currentVertex
+    // using parents array
+    private  void printPath(int currentVertex,
+                                  int[] parents)
+    {
+
+        // Base case : Source node has
+        // been processed
+        if (currentVertex == NO_PARENT)
         {
-            int dist[] = new int[V];
-            Boolean sptSet[] = new Boolean[V];
-            for (int i = 0; i < V; i++) {
-                dist[i] = Integer.MAX_VALUE;
-                sptSet[i] = false;
-            }
-
-            dist[src] = 0;
-
-            for (int count = 0; count < V - 1; count++) {
-                int u = minDistance(dist, sptSet);
-
-                sptSet[u] = true;
-
-                for (int v = 0; v < V; v++)
-                    if (!sptSet[v] && graph[u][v] != 0 &&
-                            dist[u] != Integer.MAX_VALUE && dist[u] + graph[u][v] < dist[v])
-                        dist[v] = dist[u] + graph[u][v];
-            }
-
-            // print the constructed distance array
-//            printSolution(dist, V);
+            return;
         }
+        printPath(parents[currentVertex], parents);
+        Log.e(" " , currentVertex + " ");
+        path.add(currentVertex);
+    }
 
 
 
 }
+
+// This code is contributed by Harikrishnan Rajan
